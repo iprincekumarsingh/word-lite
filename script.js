a2e7d0ebf4f7ddb395e2c974236ff5c2a29c4d08
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2021-08-23 10:17:51 +0530

    alert msg added

--- script.js
+++ script.js
@@ -3,7 +3,16 @@
    
 
      document.getElementById("previewtext").classList.toggle("upperCase")
-     document.getElementById("activated_btn_Upper").classList.toggle("active-btn")
+     // document.getElementById("activated_btn_Upper").classList.toggle("active-btn")
+     
+ document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+ Text converted into UpperCase
+</div>`)
+
+setTimeout(() => {
+     document.getElementById("alertmsg").remove();
+
+}, 2000);
 
      // var ccc = document.getElementById("ptext").value = c
      // console.log(ccc);
@@ -30,31 +39,72 @@
      document.getElementById("ptext").value = ""
      document.getElementById("previewtext").innerText = ""
      document.getElementById("counttext").innerText=""+0+" Word"
+     document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+     Text Cleared 
+    </div>`)
+    
+    setTimeout(() => {
+         document.getElementById("alertmsg").remove();
+    
+    }, 2000);
      }
      function darkmode(){
           document.body.classList.toggle("dark-mode")
           document.getElementById("nav").classList.toggle("nav-dark")
+          document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+        Theme Changed
+         </div>`)
+         
+         setTimeout(() => {
+              document.getElementById("alertmsg").remove();
+         
+         }, 2000);
      }
      function bold(){
           var boldtext = document.getElementById("ptext")
           boldtext.classList.toggle("boldfont")
           document.getElementById("previewtext").classList.toggle("boldfont")
-          document.getElementById("activated_btn_Bold").classList.toggle("active-btn")
+          // document.getElementById("activated_btn_Bold").classList.toggle("active-btn")
+          document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+          Text converted into Bold
+         </div>`)
+         
+         setTimeout(() => {
+              document.getElementById("alertmsg").remove();
+         
+         }, 2000);
 
      }
      function italic(){
           document.getElementById("ptext").classList.toggle("italicFont")
           document.getElementById("previewtext").classList.toggle("italicFont")
-          document.getElementById("activated_btn_Italic").classList.toggle("active-btn")
+          // document.getElementById("activated_btn_Italic").classList.toggle("active-btn")
 
-          activated_btn_Italic
-
+          // activated_btn_Italic
+          document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+          Text converted into Italic
+         </div>`)
+         
+         setTimeout(() => {
+              document.getElementById("alertmsg").remove();
+         
+         }, 2000);
+     
      }
+     
      function lCase(){
           document.getElementById("ptext").classList.toggle("lowerCase")
           document.getElementById("previewtext").classList.toggle("lowerCase")
-          document.getElementById("activated_btn_Lower").classList.toggle("active-btn")
-          document.getElementById("activated_btn_Lower").innerText="Active"
+          // document.getElementById("activated_btn_Lower").classList.toggle("active-btn")
+          // document.getElementById("activated_btn_Lower").innerText="Active"
+          document.getElementById("nav").insertAdjacentHTML("afterend",`<div id="alertmsg" class="alert alert-success" role="alert">
+          Text converted into LowerCase
+         </div>`)
+         
+         setTimeout(() => {
+              document.getElementById("alertmsg").remove();
+         
+         }, 2000);
           
           // document.getElementById("previewtext").classList.toggle("lowerCase")
      }
